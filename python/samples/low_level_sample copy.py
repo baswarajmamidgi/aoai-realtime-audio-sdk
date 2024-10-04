edc470180715419bc1277b809b831611f1e595e0
baswarajmamidgi <baswarajmamidgi@gmail.com>
2024-10-04 09:34:40 +0530

    stable

--- python/samples/low_level_sample copy.py
+++ python/samples/low_level_sample copy.py
@@ -30,170 +30,6 @@ from rtclient import (
 
 
 
-async def receive_messages(client: RTLowLevelClient):
-    while not client.closed:
-        message = await client.recv()
-        if message is None:
-            continue
-        match message.type:
-            case "session.created":
-                print("Session Created Message")
-                print(f"  Model: {message.session.model}")
-                print(f"  Session Id: {message.session.id}")
-                pass
-            case "error":
-                print("Error Message")
-                print(f"  Error: {message.error}")
-                pass
-            case "input_audio_buffer.committed":
-                print("Input Audio Buffer Committed Message")
-                print(f"  Item Id: {message.item_id}")
-                pass
-            case "input_audio_buffer.cleared":
-                print("Input Audio Buffer Cleared Message")
-                pass
-            case "input_audio_buffer.speech_started":
-                print("Input Audio Buffer Speech Started Message")
-                print(f"  Item Id: {message.item_id}")
-                print(f"  Audio Start [ms]: {message.audio_start_ms}")
-                pass
-            case "input_audio_buffer.speech_stopped":
-                print("Input Audio Buffer Speech Stopped Message")
-                print(f"  Item Id: {message.item_id}")
-                print(f"  Audio End [ms]: {message.audio_end_ms}")
-                pass
-            case "conversation.item.created":
-                print("Conversation Item Created Message")
-                print(f"  Id: {message.item.id}")
-                print(f"  Previous Id: {message.previous_item_id}")
-                if message.item.type == "message":
-                    print(f"  Role: {message.item.role}")
-                    for index, content in enumerate(message.item.content):
-                        print(f"  [{index}]:")
-                        print(f"    Content Type: {content.type}")
-                        if content.type == "input_text" or content.type == "text":
-                            print(f"  Text: {content.text}")
-                        elif content.type == "input_audio" or content.type == "audio":
-                            print(f"  Audio Transcript: {content.transcript}")
-                pass
-            case "conversation.item.truncated":
-                print("Conversation Item Truncated Message")
-                print(f"  Id: {message.item_id}")
-                print(f" Content Index: {message.content_index}")
-                print(f"  Audio End [ms]: {message.audio_end_ms}")
-            case "conversation.item.deleted":
-                print("Conversation Item Deleted Message")
-                print(f"  Id: {message.item_id}")
-            case "conversation.item.input_audio_transcription.completed":
-                print("Input Audio Transcription Completed Message")
-                print(f"  Id: {message.item_id}")
-                print(f"  Content Index: {message.content_index}")
-                print(f"  Transcript: {message.transcript}")
-            case "conversation.item.input_audio_transcription.failed":
-                print("Input Audio Transcription Failed Message")
-                print(f"  Id: {message.item_id}")
-                print(f"  Error: {message.error}")
-            case "response.created":
-                print("Response Created Message")
-                print(f"  Response Id: {message.response.id}")
-                print("  Output Items:")
-                for index, item in enumerate(message.response.output):
-                    print(f"  [{index}]:")
-                    print(f"    Item Id: {item.id}")
-                    print(f"    Type: {item.type}")
-                    if item.type == "message":
-                        print(f"    Role: {item.role}")
-                        match item.role:
-                            case "system":
-                                for content_index, content in enumerate(item.content):
-                                    print(f"    [{content_index}]:")
-                                    print(f"      Content Type: {content.type}")
-                                    print(f"      Text: {content.text}")
-                            case "user":
-                                for content_index, content in enumerate(item.content):
-                                    print(f"    [{content_index}]:")
-                                    print(f"      Content Type: {content.type}")
-                                    if content.type == "input_text":
-                                        print(f"      Text: {content.text}")
-                                    elif content.type == "input_audio":
-                                        print(f"      Audio Data Length: {len(content.audio)}")
-                            case "assistant":
-                                for content_index, content in enumerate(item.content):
-                                    print(f"    [{content_index}]:")
-                                    print(f"      Content Type: {content.type}")
-                                    print(f"      Text: {content.text}")
-                    elif item.type == "function_call":
-                        print(f"    Call Id: {item.call_id}")
-                        print(f"    Function Name: {item.name}")
-                        print(f"    Parameters: {item.arguments}")
-                    elif item.type == "function_call_output":
-                        print(f"    Call Id: {item.call_id}")
-                        print(f"    Output: {item.output}")
-            case "response.done":
-                print("Response Done Message")
-                print(f"  Response Id: {message.response.id}")
-                if message.response.status_details:
-                    print(f"  Status Details: {message.response.status_details.model_dump_json()}")
-                break
-            case "response.output_item.added":
-                print("Response Output Item Added Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item.id}")
-            case "response.output_item.done":
-                print("Response Output Item Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item.id}")
-
-            case "response.content_part.added":
-                print("Response Content Part Added Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item_id}")
-            case "response.content_part.done":
-                print("Response Content Part Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  ItemPart Id: {message.item_id}")
-            case "response.text.delta":
-                print("Response Text Delta Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Text: {message.delta}")
-            case "response.text.done":
-                print("Response Text Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Text: {message.text}")
-            case "response.audio_transcript.delta":
-                print("Response Audio Transcript Delta Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item_id}")
-                print(f"  Transcript: {message.delta}")
-            case "response.audio_transcript.done":
-                print("Response Audio Transcript Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item_id}")
-                print(f"  Transcript: {message.transcript}")
-            case "response.audio.delta":
-                print("Response Audio Delta Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item_id}")
-                print(f"  Audio Data Length: {len(message.delta)}")
-            case "response.audio.done":
-                print("Response Audio Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Item Id: {message.item_id}")
-            case "response.function_call_arguments.delta":
-                print("Response Function Call Arguments Delta Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Arguments: {message.delta}")
-            case "response.function_call_arguments.done":
-                print("Response Function Call Arguments Done Message")
-                print(f"  Response Id: {message.response_id}")
-                print(f"  Arguments: {message.arguments}")
-            case "rate_limits.updated":
-                print("Rate Limits Updated Message")
-                print(f"  Rate Limits: {message.rate_limits}")
-            case _:
-                print("Unknown Message")
-
-
 def get_env_var(var_name: str) -> str:
     value = os.environ.get(var_name)
     if not value:
@@ -201,33 +37,13 @@ def get_env_var(var_name: str) -> str:
     return value
 
 
-async def with_azure_openai(audio_file_path: str):
-    endpoint = get_env_var("AZURE_OPENAI_ENDPOINT")
-    key = get_env_var("AZURE_OPENAI_API_KEY")
-    deployment = get_env_var("AZURE_OPENAI_DEPLOYMENT")
-    async with RTLowLevelClient(
-        endpoint, key_credential=AzureKeyCredential(key), azure_deployment=deployment
-    ) as client:
-        await client.send(
-            SessionUpdateMessage(
-                session=SessionUpdateParams(
-                    turn_detection=ServerVAD(type="server_vad"),
-                    input_audio_transcription=InputAudioTranscription(model="whisper-1"),
-                )
-            )
-        )
-
-        await asyncio.gather(send_audio(client, audio_file_path), receive_messages(client))
-
-
-
 
 class AudioRecorder:
     def __init__(self):
         self.is_recording = False
         self.frames = []
-        self.sample_rate = 16000
-        self.chunk = 4800
+        self.sample_rate = 24000
+        self.chunk = 1024
         self.audio = pyaudio.PyAudio()
         self.stream = None
 
@@ -346,7 +162,6 @@ class Application(tk.Tk):
                 if message.type == "response.audio.delta":
                     audio_buffer.append(base64.b64decode(message.delta))
                 elif message.type == "response.done":
-                    print("Response done message")
                     if audio_buffer:
                         audio_data = b''.join(audio_buffer)
                         self.after(0, self.play_audio, audio_data)
